6ee3702efd2d1afa9f3e98a5bc840ca2c4858e32
Jacob Tarun <jacobtarun782@outlook.com>
2025-09-08 10:05:06 +0800

    Organize routes

---
+++ routes/admin.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\HandleGuest;
+use App\Models\Product;
+use App\Models\User;
+use Inertia\Inertia;
+
+
+Route::middleware(['auth', HandleGuest::class])->group(function () {
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+
+        Route::get('/', function() {
+            return redirect('admin/products');
+        });
+
+        Route::get('products', function (Request $request) {
+            return Inertia::render('admin/Products', ['products' => Product::orderBy('created_at', 'desc')->get()]);
+        })->name('products');
+        Route::post('products', [ProductController::class, 'store']
+        )->name('products.create');
+        Route::patch('products', [ProductController::class, 'update']
+        )->name('products.update');
+        Route::delete('products', [ProductController::class, 'delete']
+        )->name('products.delete');
+
+        Route::get('orders', function (Request $request) {
+            return Inertia::render('admin/Orders');
+        })->name('orders');
+
+        Route::get('users', function (Request $request) {
+            return Inertia::render('admin/Users', ['users' => User::orderBy('created_at', 'desc')->get()]);
+        })->name('users');
+        Route::post('users', [UserController::class, 'store']
+        )->name('users.create');
+        Route::patch('users', [UserController::class, 'update']
+        )->name('users.update');
+        Route::delete('users', [UserController::class, 'delete']
+        )->name('users.delete');
+    });
+});
