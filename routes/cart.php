476fd10830ad25d54edd4010b86cd3e2eac93f17
Jacob Tarun <jacobtarun782@outlook.com>
2025-09-08 17:50:40 +0800

    Cart routes

--- routes/cart.php
+++ routes/cart.php
@@ -5,5 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
 
+    Route::get('cart', function () {
+        return to_route('home');
+    });
     Route::post('cart', [CartController::class, 'store'])->name('cart.store');
+    Route::delete('cart', [CartController::class, 'delete'])->name('cart.delete');
 });
